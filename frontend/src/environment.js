0447a86b5b4e2ff5c526a949a824b2b8ed3d106e
Vibhor Mishra <vibhormishra0705@gmail.com>
2026-08-27 23:57:50 +0530

    Server Production

--- frontend/src/environment.js
+++ frontend/src/environment.js
@@ -1,4 +1,4 @@
-let IS_PROD = true;
-const server = IS_PROD ? "https://sampark-665u.onrender.com" : "http://localhost:8000";
+let IS_PROD1 = true;
+const server = IS_PROD1 ? "https://sampark-665u.onrender.com" : "http://localhost:8000";
 
 export default server;
